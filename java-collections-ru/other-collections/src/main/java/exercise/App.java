723759ae77a32e78bb2590906b2be81bd246ff92
@hexlet/cli <support@hexlet.io>
2022-10-24 14:44:06 +0400

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import java.util.*;
+
+// BEGIN
+class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> dict1, Map<String, Object> dict2) {
+
+        Set<String> allKeys = new LinkedHashSet<>();
+        allKeys.addAll(dict1.keySet());
+        allKeys.addAll(dict2.keySet());
+
+        LinkedHashMap<String, String> difference = new LinkedHashMap<>();
+
+        for (var key : allKeys) {
+            if (dict1.containsKey(key) && dict2.containsKey(key)) {
+                var dict1Value = dict1.get(key);
+                var dict2Value = dict2.get(key);
+                if (dict1Value.equals(dict2Value)) {
+                    difference.put(key, "unchanged");
+                } else {
+                    difference.put(key, "changed");
+                }
+            } else if (!dict1.containsKey(key) && dict2.containsKey(key)) {
+                difference.put(key, "added");
+            } else if (dict1.containsKey(key) && !dict2.containsKey(key)) {
+                difference.put(key, "deleted");
+            }
+        }
+
+        return difference;
+
+    }
+}
+//END
