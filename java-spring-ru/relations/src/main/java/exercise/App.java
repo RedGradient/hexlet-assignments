5f71408e9c7c4489ccc5576102d07755c59d14f4
@hexlet/cli <support@hexlet.io>
2023-02-01 19:41:41 +0400

    submit java-spring-ru/relations

--- java-spring-ru/relations/src/main/java/exercise/App.java
+++ java-spring-ru/relations/src/main/java/exercise/App.java
@@ -3,16 +3,11 @@ package exercise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         // Запускаем приложение
-//        var app = new SpringApplication(App.class);
-//        app.setDefaultProperties(Collections.singletonMap("server.port", "5001"));
          SpringApplication.run(App.class, args);
-//        app.run();
     }
 }
