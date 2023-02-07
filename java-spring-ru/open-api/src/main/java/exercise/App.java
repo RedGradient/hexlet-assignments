8f3fdb62d5cbba1debde1c608b559722cefcb35d
@hexlet/cli <support@hexlet.io>
2023-02-07 17:53:58 +0400

    submit java-spring-ru/open-api

---
+++ java-spring-ru/open-api/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.util.comparator.Comparators;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+// Указываем общую информацию о нашем API
+@OpenAPIDefinition(
+    info = @Info(
+        // Название
+        title = "Users API",
+        // Версия
+        version = "1.0",
+        // Описание
+        description = "Information about users"
+    )
+)
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
